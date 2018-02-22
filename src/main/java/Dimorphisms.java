67075ba8de3f9edfcc6523f4ed05b156149199b2
carles <carlesbalsellsrodas@hotmail.com>
2018-02-22 18:33:36 +0100

    executable created

---
+++ src/main/java/Dimorphisms.java
@@ -0,0 +1,9 @@
+public class Dimorphisms {
+    public static void main(String args[]) {
+        javax.swing.SwingUtilities.invokeLater (
+                () -> {
+                    System.out.println("Hello World");
+                    
+                });
+    }
+}
