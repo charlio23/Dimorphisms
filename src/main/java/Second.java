91fb1c96f5f2cc4167944e73d29f494026646487
carles <carlesbalsellsrodas@hotmail.com>
2018-03-01 16:56:42 +0100

    View Controller - switch between views implemented

---
+++ src/main/java/Second.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Second {
+
+    private ViewController viewController;
+
+    public void setViewController(ViewController viewController) {
+        this.viewController = viewController;
+    }
+}
